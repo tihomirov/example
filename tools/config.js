341d48d901bde2a96fc97dfce1a7cee530067094
GregoryHouse <strelbanozem@mail.ru>
2016-03-29 23:35:53 +0300

    Added files via upload

---
+++ tools/config.js
@@ -0,0 +1,8 @@
+/**
+ * Babel Starter Kit | https://github.com/kriasoft/babel-starter-kit
+ * Copyright (c) Konstantin Tarkus <hello@tarkus.me> | The MIT License
+ */
+
+export default {
+  rootDir: '/app/',
+};
